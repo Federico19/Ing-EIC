a19cb4f683e49c8d669e1b9ac63ab49107d433e9
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2022-06-19 13:20:54 -0300

    Add Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,50 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest/globals": true,
+    "node": true
   },
-  extends: [
-    'airbnb-base',
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
-  rules: {
-  },
-};
+  "plugins": [
+    "react", "jest"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+        2
+      ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": "warn",
+    "react/prop-types": 0
+  }
+}
